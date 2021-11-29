40e77c5510ee4782ab3c520827e3136fb6741195
SharmaTrisha <sharmatrisha315@gmail.com>
2021-11-29 23:50:24 +0530

    Create comic_mailer.php

---
+++ comic_mailer.php
@@ -0,0 +1,50 @@
+<?php
+require_once './functions.php';
+
+require_once '../navigation/db.php';
+$data  = file_get_contents( 'php://input' );
+$data  = json_decode( $data, true );
+$email = $data['email'];
+if ( ! filter_var( $email, FILTER_VALIDATE_EMAIL )) {
+	http_response_code( 400 );
+	returnResponse( 'Enter Valid Email' );
+	exit();
+}
+$OTP = genetateOTP();
+if ( $con ) {
+	try {
+		$stmt = $con->prepare( 'SELECT is_activated FROM `subscribers` WHERE email=?' );
+		$stmt->bind_param( 's', $email );
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->bind_result( $is_activated );
+		$stmt->fetch();
+		$numRows = $stmt->num_rows;
+		if ($is_activated === 1) {
+			http_response_code( 201 );
+			returnResponse( 'Already Registered, please try different email address' );
+			exit();
+		}
+		if ($numRows === 0) {
+			$email_hash = md5( $email );
+			$stmt       = $con->prepare( 'INSERT INTO `subscribers` (`email`, `otp`, `email_hash`) VALUES (?, ?, ?)' );
+			$stmt->bind_param( 'sis', $email, $OTP, $email_hash );
+			$stmt->execute();
+		} else {
+			$stmt = $con->prepare( 'UPDATE `subscribers` SET `otp`=? WHERE `email`=?' );
+			$stmt->bind_param( 'is', $OTP, $email );
+			$stmt->execute();
+		}
+		http_response_code( 200 );
+		returnResponse( 'Check your inbox for OTP' );
+		$mail_text    = "Welcome to Ping XKCD.\nYour OTP for email validation is $OTP.\nEnjoy!";
+		$mail_subject = 'Ping XKCD Comic  - OTP';
+		sendMail( $email, $mail_subject, $mail_text );
+		exit();
+	} catch (\Throwable $th) {
+		echo $th;
+		http_response_code( 500 );
+		returnResponse( 'Something went wrong' );
+		exit();
+	}
+}
